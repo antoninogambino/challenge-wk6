114fab07c4a7c78358acc8b1a89de8c74e5bb9b0
antoninogambino <112243266+antoninogambino@users.noreply.github.com>
2023-04-23 14:25:09 +0100

    start button event listener

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -3,3 +3,6 @@ let currentQuestionIndex = 0;
 let userScore = 0;
 let remainingTime = 60;
 let timer;
+
+// Add an event listener for the start button
+document.getElementById('start').addEventListener('click', function()

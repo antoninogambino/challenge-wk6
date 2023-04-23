7601d8d6d9e93504f2d4b58e023df6a86815f55c
antoninogambino <112243266+antoninogambino@users.noreply.github.com>
2023-04-23 14:21:10 +0100

    added questions for the quiz

---
+++ starter/assets/js/questions.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+        question: "What is the capital of France?",
+        answers: ["Paris", "Berlin", "Rome", "Madrid"],
+        correctAnswer: "Paris"
+      },
+      {
+        question: "What is the capital of Japan?",
+        answers: ["Tokyo", "Beijing", "Seoul", "Bangkok"],
+        correctAnswer: "Tokyo"
+      },
+      {
+        question: "What is the capital of Australia?",
+        answers: ["Sydney", "Melbourne", "Canberra", "Brisbane"],
+        correctAnswer: "Canberra"
+      },
+      {
+        question: "What is the capital of Brazil?",
+        answers: ["Brasília", "Rio de Janeiro", "São Paulo", "Salvador"],
+        correctAnswer: "Brasília"
+      },
+      {
+        question: "What is the capital of Egypt?",
+        answers: ["Cairo", "Alexandria", "Giza", "Luxor"],
+        correctAnswer: "Cairo"
+      }
+];
